e2cfa2cb424527bc66ff0fcf3d5113de61e594d1
nadacoat <nadacoat@gmail.com>
2021-09-22 19:42:56 -0400

    this is the scrip I wrote

---
+++ variablescript.py
@@ -0,0 +1,32 @@
+var1 = 10
+var2 = -1 
+var3 = 1 
+var4 = 9
+
+ans1 = 2 * var1 - 1
+ans2 = 2 * var2 - 1
+ans3 = 2 * var3 - 1
+ans4 = 2 * var4 - 1
+
+yes = "yes"
+no = "no"
+
+if ans1 < 17:
+    print(yes)
+else:
+    print(no)
+
+if ans2 < 17:
+    print(yes)
+else:
+    print(no)
+        
+if ans3 < 17:
+    print(yes)
+else:
+    print(no)
+    
+if ans4 < 17:
+    print(yes)
+else:
+    print(no)
